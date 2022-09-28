1265cf7380128fb55ccf72f6caefef4bf5d7f30a
Mario De La Torre Ochoa <m.delatorreochoa@gmail.com>
2022-09-28 17:00:29 +0200

    barakaldo

--- aperturas/app.js
+++ aperturas/app.js
@@ -1,5 +1,5 @@
 const countdown =() =>{
-    const countDate = new Date('September 30, 2022 18:30:00').getTime();
+    const countDate = new Date('November 26, 2022 10:00:00').getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
