ef2479c8381e9fcc90fdfc13026df9f7305b65bb
Mario De La Torre Ochoa <m.delatorreochoa@gmail.com>
2022-10-25 14:27:24 +0200

    vilanova

--- aperturas/app.js
+++ aperturas/app.js
@@ -1,5 +1,5 @@
 const countdown =() =>{
-    const countDate = new Date('November 26, 2022 10:00:00').getTime();
+    const countDate = new Date('December 02, 2022 10:00:00').getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
