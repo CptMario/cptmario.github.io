71ab4487a2b61aec67483893affb117cf631ee36
Mario De La Torre Ochoa <m.delatorreochoa@gmail.com>
2024-04-02 09:59:31 +0200

    bilbao

--- aperturas/app.js
+++ aperturas/app.js
@@ -1,5 +1,5 @@
 const countdown =() =>{
-    const countDate = new Date('March 27, 2024 18:00:00').getTime();
+    const countDate = new Date('May 17, 2024 18:00:00').getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
