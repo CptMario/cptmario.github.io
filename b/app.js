562cd9ba13d1b366275a80e1a4adba564a5d9410
Mario De La Torre Ochoa <m.delatorreochoa@gmail.com>
2025-02-17 12:29:52 +0100

    FRANQUIS

--- b/app.js
+++ b/app.js
@@ -1,4 +1,4 @@
-const names = ['Thomas Mendonça', 'Julien Davy', 'Mathieu Lebeuf', 'Nicolas Paquay' , 'Florent Williamson']; // Array con los nombres
+const names = ['Jean François Laurent', 'Thierry Espinosa']; // Array con los nombres
 const nameElement = document.getElementById('name'); // Elemento h2 donde se mostrarán los nombres
 let currentIndex = 0; // Índice actual en el array
 const numParticles = 50; // Número de partículas
