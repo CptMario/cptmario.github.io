e184702d64b6f4e8002c3d1d58a6f90a6038bd9a
Mario De La Torre Ochoa <m.delatorreochoa@gmail.com>
2025-07-14 10:50:19 +0200

    Update app.js

--- aperturas/app.js
+++ aperturas/app.js
@@ -66,7 +66,7 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 const countdown =() =>{
-    const countDate = new Date('Jul 11, 2025 18:00:00').getTime();
+    const countDate = new Date('Jul 25, 2025 18:00:00').getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
